23a2629db9ec3ae8a754a534e89cc5399afd608e
Your Name <you@example.com>
2024-07-10 13:38:50 -0500

    created the DbContext file

--- obj/Debug/net8.0/fsiplanner-backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/fsiplanner-backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fsiplanner-backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fe8d0de8c1f4afd0179c65c069036767e5ef76f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bec677c0e4896a4db7b9397e300d32d884079df")]
 [assembly: System.Reflection.AssemblyProductAttribute("fsiplanner-backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fsiplanner-backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
