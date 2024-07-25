7e5fee4d7cdb85dbb2bc90fee098f0d5ce24eec9
Your Name <you@example.com>
2024-07-24 22:12:46 -0500

    Created the life controller

---
+++ Controllers/LifeController.cs
@@ -0,0 +1,96 @@
+using System.Security.Claims;
+using fsiplanner_backend.Models;
+using fsiplanner_backend.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace fsiplanner_backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class LifeController : ControllerBase
+    {
+        private readonly ILogger<LifeController> _logger;
+        private readonly ILifeRepository _lifeRepository;
+        public LifeController(ILogger<LifeController> logger, ILifeRepository repository)
+        {
+            _logger = logger;
+            _lifeRepository = repository;
+        }
+
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<IEnumerable<Life>> GetAllLife()
+        {
+            return Ok(_lifeRepository.GetAllLife());
+        }
+
+        [HttpGet]
+        [Route("{username}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<ActionResult<IEnumerable<Life>>> GetLifeByUsername(string username)
+        {
+            IEnumerable<Life> life = (IEnumerable<Life>) await _lifeRepository.GetLifeByUsername(username);
+
+            if(life == null || !life.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(life);
+        }
+
+        [HttpGet]
+        [Route("{lifeId:int}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<Life> GetLifeById(int lifeId)
+        {
+            var life = _lifeRepository.GetLifeById(lifeId);
+            if(life == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(life);
+        }
+
+        [HttpPost]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<Life> CreateLife(Life life)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            life.UserId = int.Parse(User.FindFirst(ClaimTypes.NameIdentifier)!.Value);
+            
+            var newLife = _lifeRepository.CreateLife(life);
+            return Created(nameof(GetLifeById), newLife);
+        }
+
+        [HttpPut]
+        [Route("{lifeId:int}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult<Life> UpdateLife(Life life)
+        {
+            if(!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            return Ok(_lifeRepository.UpdateLife(life));
+        }
+
+        [HttpDelete]
+        [Route("{lifeId:int}")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public ActionResult DeleteLife(int lifeId)
+        {
+            _lifeRepository.DeleteLife(lifeId);
+            return NoContent();
+        }
+
+    }
+}
